1ccf4b99e72896143a47939556ab5ca107af22da
Ruslan Fatykhov <rfatykhov@Macintosh.local>
2022-02-28 15:13:46 +0300

    Navigation bar

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,20 +1,25 @@
 import houses from "../houses.js";
 import Head from "next/head";
+import Layout from '../components/Layout';
 
 export default function House(props) {
-	return (
-		<div>
-			<Head>
-				<title>{props.house.title}</title>
-			</Head>
-			<img src={props.house.picture} width="100%" alt="House picture" />
-			<p>
-				{props.house.type} - {props.house.town}
-			</p>
-			<p>{props.house.title}</p>
-		</div>
-	);
-}
+    return (
+      <Layout
+        content={
+          <div>
+            <Head>
+              <title>{props.house.title}</title>
+            </Head>
+            <img src={props.house.picture} width="100%" alt="House picture" />
+            <p>
+              {props.house.type} - {props.house.town}
+            </p>
+            <p>{props.house.title}</p>
+          </div>
+        }
+      />
+    )
+  }
 
 export async function getServerSideProps({ query }) {
 	const { id } = query;
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,28 @@
 import houses from "./houses.js";
 import House from "./components/House";
+import Layout from "./components/Layout";
+
+const content = (
+	<div>
+		<h2>Places to stay</h2>
+
+		<div className="houses">
+			{houses.map((house, index) => {
+				return <House key={index} {...house} />;
+			})}
+		</div>
+
+		<style jsx>{`
+			.houses {
+				display: grid;
+				grid-template-columns: 49% 49%;
+				grid-template-rows: 300px 300px;
+				grid-gap: 2%;
+			}
+		`}</style>
+	</div>
+);
 
 export default function Home() {
-	return (
-		<div>
-			<h2>Places to stay</h2>
-
-			<div className="houses">
-				{houses.map((house, index) => {
-					return <House key={index} {...house} />;
-				})}
-			</div>
-
-			<style jsx>{`
-				.houses {
-					display: grid;
-					grid-template-columns: 49% 49%;
-					grid-template-rows: 300px 300px;
-					grid-gap: 2%;
-				}
-			`}</style>
-		</div>
-	);
+	return <Layout content={content} />;
 }
