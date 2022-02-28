cbf2a14e7e823b19396a04f5b3fdc2180f338eb4
Ruslan Fatykhov <rufatykhov@gmail.com>
2022-02-28 15:19:52 +0300

    Add sidebar template

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,25 +1,41 @@
 import houses from "../houses.js";
 import Head from "next/head";
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 
 export default function House(props) {
-    return (
-      <Layout
-        content={
-          <div>
-            <Head>
-              <title>{props.house.title}</title>
-            </Head>
-            <img src={props.house.picture} width="100%" alt="House picture" />
-            <p>
-              {props.house.type} - {props.house.town}
-            </p>
-            <p>{props.house.title}</p>
-          </div>
-        }
-      />
-    )
-  }
+	return (
+		<Layout
+			content={
+				<div className="container">
+					<Head>
+						<title>{props.house.title}</title>
+					</Head>
+					<article>
+						<img src={props.house.picture} width="100%" alt="House picture" />
+						<p>
+							{props.house.type} - {props.house.town}
+						</p>
+						<p>{props.house.title}</p>
+					</article>
+					<aside></aside>
+
+					<style jsx>{`
+						.container {
+							display: grid;
+							grid-template-columns: 60% 40%;
+							grid-gap: 30px;
+						}
+
+						aside {
+							border: 1px solid #ccc;
+							padding: 20px;
+						}
+					`}</style>
+				</div>
+			}
+		/>
+	);
+}
 
 export async function getServerSideProps({ query }) {
 	const { id } = query;
